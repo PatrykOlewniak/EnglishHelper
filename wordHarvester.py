e20cbef6c15c2e33b2a9a6ffb338f817ffae7bc3
Patryk Olewniak <p.olewniak@gmail.com>
2017-08-15 23:21:52 +0200

    added file for kindle sync , EnglishPolishWord harvester - basic

---
+++ wordHarvester.py
@@ -0,0 +1,30 @@
+from urllib import urlopen
+from bs4 import BeautifulSoup
+
+
+class dikiTranslator:
+    url = "https://www.diki.pl"
+    searchingSuffix= "/slownik-angielskiego?q="
+
+    @staticmethod
+    def searchWordPartOfSpeech(word):
+        html = urlopen((dikiTranslator.url+dikiTranslator.searchingSuffix+word))
+        bsObj = BeautifulSoup(html.read(), "html5lib")
+        nameList = bsObj.findAll("span", {"class": "partOfSpeech"})
+        return nameList[0].get_text()
+
+    @staticmethod
+    def searchWordPolishMeaning(word):
+        html = urlopen((dikiTranslator.url+dikiTranslator.searchingSuffix+word))
+        bsObj = BeautifulSoup(html.read(), "html5lib")
+        nameList = bsObj.findAll("span", {"class": "hw"})
+        #strip for removing spaces and tabs before
+        return (nameList[0].get_text()).strip()
+
+
+
+
+print dikiTranslator.searchWordPolsihPartOfSpeech("task")
+
+
+print dikiTranslator.searchWordPolishMeaning("task")
