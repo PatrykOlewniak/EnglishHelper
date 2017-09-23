ac9baccea968ea69da24980bdc344c99023d4cd5
Patryk Olewniak <p.olewniak@gmail.com>
2017-09-23 22:14:05 +0200

    draft for kindleSync

--- kindleSync.py
+++ kindleSync.py
@@ -1,1 +1,17 @@
 #TODO: connect to kindle db (sql lite) and import dictionary
+
+class KindleSync(object):
+    def _connectToKindle(self):
+        pass
+
+    def _findKindle(self):
+        pass
+
+    def _connectToKindleDataBase(self):
+        pass
+
+    def getEnglishWords(self):
+        pass
+
+    def copyKindleDataBase(self):
+        pass
